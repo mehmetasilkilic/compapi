eef6ba77e3a6c3813c5c7c39573422b589887de9
mehmetasilkilic <87938113+mehmetasilkilic@users.noreply.github.com>
2022-02-08 10:26:03 +0300

    home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,17 @@
 </template>
 
 <script>
-
 export default {
   name: "Home",
+  setup() {
+    let name = "mario";
+    let age = 30;
+
+    const handleClick = () => {
+      console.log("clicked");
+    };
+
+    return { name, age, handleClick };
+  },
 };
 </script>
