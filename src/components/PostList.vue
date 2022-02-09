13932bfe72e49d997aaf857c0afc3176797b464d
mehmetasilkilic <87938113+mehmetasilkilic@users.noreply.github.com>
2022-02-09 09:55:10 +0300

    composables

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -9,6 +9,7 @@
 <script>
 import { onMounted, onUnmounted, onUpdated } from "vue";
 import SinglePost from "./SinglePost.vue";
+
 export default {
   props: ["posts"],
   components: { SinglePost },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,26 @@
 <template>
   <div class="home">
-    <PostList v-if="showPosts" :posts="posts" />
-    <button @click="showPosts = !showPosts">Toggle Posts</button>
-    <button @click="posts.pop()">Delete a Post</button>
+    <h1>Home</h1>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>Loading...</div>
   </div>
 </template>
 
 <script>
 import PostList from "../components/PostList.vue";
-import { ref } from "vue";
+import getPosts from "../composables/getPosts";
 
 export default {
   name: "Home",
   components: { PostList },
   setup() {
-    const posts = ref([
-      {
-        title: "welcome to the blog",
-        body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit labore similique magni officiis consequatur iusto sequi, harum, quaerat qui possimus amet odio aliquam quibusdam excepturi reiciendis numquam, fuga provident quasi!  ",
-        id: 1,
-      },
-      {
-        title: "top 5 css tips",
-        body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit labore similique magni officiis consequatur iusto sequi, harum, quaerat qui possimus amet odio aliquam quibusdam excepturi reiciendis numquam, fuga provident quasi!",
-        id: 2,
-      },
-    ]);
-    const showPosts = ref(true);
+    const { posts, error, load } = getPosts();
+    load();
 
-    return { posts, showPosts };
+    return { posts, error };
   },
 };
 </script>
